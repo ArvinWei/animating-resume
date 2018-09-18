fe09664a1dd2ce4b28cd8b15bd20499d1e7ce15a
ArvinWei <weiyangle@qq.com>
2018-09-18 16:39:19 +0800

    修改时间调试

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ function writeCss(prefix, code, fn){
       window.clearInterval(id)
       fn && fn.call()
     }
-  }, 10)
+  }, 50)
 }
 function writeMarkdown(markdown, fn){
   let domPaper = document.querySelector('#paper>.content')
